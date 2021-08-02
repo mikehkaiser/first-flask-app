769fac9243ec804a44f9f3590acf559935fb575b
mikehkaiser <mikehkaiser@gmail.com>
2021-08-01 22:03:58 -0500

    first commit. most basic applications

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi, {}'.format(name)
